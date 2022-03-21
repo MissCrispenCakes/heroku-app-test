41034128f0d6a333842df592db51c9e433305e92
MissCrispenCakes <10444589+MissCrispenCakes@users.noreply.github.com>
2022-03-21 23:40:52 +0000

    scripts

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+# request, flash
+# from wtforms import Form, TextField, TextAreaField, validators, StringField
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/") 
 def index():
-
+   
     # Load current count
     f = open("count.txt", "r")
     count = int(f.read())
@@ -17,11 +19,18 @@ def index():
     f = open("count.txt", "w")
     f.write(str(count))
     f.close()
+  
+    # Render HTML with count variable
+    return render_template("index.html", count=count)
 
-    msg = input("Please enter a message: ", "")
-    
-  # Render HTML with count variable
-    return render_template("index.html", count=count, msg=msg)
+# ButtonPressed = 0
+# @app.route("/message", methods=["GET", "POST"])
+# def messsage():
+#     if request.method == "POST":
+#         return render_template("message.html", ButtonPressed=ButtonPressed)
+#         ButtonPressed = 1
+#     return render_template("message.html", ButtonPressed=ButtonPressed)
+        
 
 if __name__ == "__main__":
     app.run()
